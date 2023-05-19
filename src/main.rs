c423c8ff3a1115618aceee68ad6ff4d2b8484d8c
sag-desktop <sgrkl4@gmail.com>
2023-05-19 18:25:49 +0200

    Fixing the error handling of the skeleton.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 use std::env;
-use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main(){
     let args: Vec<_> = env::args().collect();
     if args.len() > 2 {
         println!("Usage: rlox [script].");
     } else if args.len() == 2 {
-        run_file(&args[1])?;
+        run_file(&args[1]);
     } else {
         run_prompt();
     }
-    Ok(())
 }
 
-fn run_file(file: &str) -> Result<(), Box<dyn Error>> {
-    let mut file = match File::open(file) {
+fn run_file(file_name: &str){
+    let mut file = match File::open(file_name) {
         Ok(file_handle) => file_handle,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => panic!("Problem opening the file: {}.  Error: {:?}", file_name, error),
     };
     let mut file_contents = String::new();
-    file.read_to_string(&mut file_contents)?;
+    file.read_to_string(&mut file_contents).expect(&format!("Error reading the file: {file_name}."));
     run(&file_contents);
-    Ok(())
 }
 
 fn run(source: &str) {
